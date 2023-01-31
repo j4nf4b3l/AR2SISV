feaaf2cde8b3b6cb6ace109ce8afad48088ad4c4
Jan <hello@janfabel.de>
2023-01-31 22:42:57 +0100

    Initial Commit

---
+++ AR2SISV/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AR2SISV
+//
+//  Created by Jan Fabel on 31.01.23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
